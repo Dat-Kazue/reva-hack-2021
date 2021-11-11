333a67ebdf230057eeaeac9e7608111ed09d0520
deondmello16 <80735760+deondmello16@users.noreply.github.com>
2021-11-11 11:49:54 +0530

    11.49

--- js code/mongoose.js
+++ js code/mongoose.js
@@ -5,3 +5,19 @@ main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect('mongodb+srv://deon:deondmello@deonps.antgg.mongodb.net/reva_hack?retryWrites=true&w=majority');
 }
+
+const blogSchema = new Schema({
+    report_id:  String,
+    user_name : String,
+    defination_c:   String, // here the user defines the type of crime committed
+    discription_of_crime : [{ body: String, date: Date }],
+    date: { type: Date, default: Date.now },
+    hidden: Boolean,
+    image: Buffer,
+    meta: {
+      report_id: Number,
+      time_:  [{type:Date,default: Date.now}]
+    }
+  });
+
+// ceating model
